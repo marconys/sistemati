15b60e056ab2888a3d22e03226902c5a9c2e8dad
marconys <marconysbatera@gmail.com>
2022-08-10 11:54:40 -0300

    Finalizando em sala produto_busca e inserindo o produto_detalhe.php

--- rodape.php
+++ rodape.php
@@ -25,7 +25,7 @@
                         contato@boizinho.com.br
                     </a>
                 </address>
-                <div class="embled-responsive embled-responive-16by9">
+                <div class="embed-responsive embed-responsive-4by3">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.7942870047204!2d-46.458731884693755!3d-23.5399001846936!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce66bf22458913%3A0xecdac462b58a9475!2sSenac%20Itaquera!5e0!3m2!1spt-BR!2sbr!4v1660051724396!5m2!1spt-BR!2sbr" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
 
@@ -33,89 +33,84 @@
         </div> <!-- Fecha área de navegação -->
         <!-- Abre área de navegação -->
         <div class="col-sm-6 col-md-4">
-            <div class="panel-footer" style="background:none;">
-            <h4>Links</h4>
-            <ul class="nav nav-pills nav-stacked">
-                <li>
-                    <a href="index.php#home" class="text-danger">
-                    <span class="glyphicon glyphicon-home" arial-hiden="true">&nbsp; Home</span>
-                </a>
-            </li>
+            <div class="panel-footer" style="background: none;">
+                <h4>Links</h4>
+                <ul class="nav nav-pills nav-stacked">
+                    <li><a href="index.php#home" class="text-danger">
+                            <span class="glyphicon glyphicon-home" aria-hidden="true">&nbsp;Home</span>
+                        </a>
+                    </li>
+                    <li><a href="index.php#destaques" class="text-danger">
+                            <span class="glyphicon glyphicon-ok-sign" aria-hidden="true">&nbsp;Destaques</span>
+                        </a>
+                    </li>
+                    <li><a href="index.php#produtos" class="text-danger">
+                            <span class="glyphicon glyphicon-cutlery" aria-hidden="true">&nbsp;Produtos</span>
+                        </a>
+                    </li>
 
-                <li>
-                    <a href="index.php#destaques" class="text-danger">
-                <span class="glyphicon glyphicon-ok-sign" arial-hiden="true">&nbsp; Destaque</span>
-                </a>
-            </li>
 
-                <li>
-                    <a href="index.php#produtos" class="text-danger">
-                <span class="glyphicon glyphicon-cutlery" arial-hiden="true">&nbsp; Produtos</span>
-                </a>
-            </li>
-                <li>
-                    <a href="index.php#contato" class="text-danger">
-                <span class="glyphicon glyphicon-envelope" arial-hiden="true">&nbsp; Contato</span>
-                </a>
-            </li>
-                <li>
-                    <a href="admin/index.php" class="text-danger">
-                <span class="glyphicon glyphicon-user" arial-hiden="true">&nbsp; Administração</span>
-                </a>
-            </li>
-            </ul>
-        </div>
-        </div>
+                    <li><a href="index.php#contato" class="text-danger">
+                            <span class="glyphicon glyphicon-envelope" aria-hidden="true">&nbsp;Contato</span>
+                        </a>
+                    </li>
+                    <li><a href="admin/index.php#" class="text-danger">
+                            <span class="glyphicon glyphicon-user" aria-hidden="true">&nbsp;Administração</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div><!-- Fecha area de navegação-->
 
         <div class="col-sd-6 col-md-4">
             <div class="panel-footer" style="background: none">
-            <h4>Contato</h4>
-            <form action="rodape_contato-envia.php" method="post" name="form-contato" id="form-contato">
-                <p>
-                    <span class="input-group">
-                        <span class="input-group-addon" id="basic-addon2">
-                            <span class="glyphicon glyphicon-user" arial-hiden="true"></span>
+                <h4>Contato</h4>
+                <form action="rodape_contato-envia.php" method="post" name="form-contato" id="form-contato">
+                    <p>
+                        <span class="input-group">
+                            <span class="input-group-addon" id="basic-addon2">
+                                <span class="glyphicon glyphicon-user" arial-hiden="true"></span>
+                            </span>
+                            <input type="text" name="nome_contato" id="nome_contato" placeholder="digite o seu nome" aria-describedby="basic-addon1" required class="form-control">
                         </span>
-                        <input type="text" name="nome_contato" id="nome_contato" placeholder="digite o seu nome" aria-describedby="basic-addon1" required class="form-control">
-                    </span>
-                </p>
-                <p>
-                    <span class="input-group">
-                        <span class="input-group-addon" id="basic-addon2">
-                            <span class="glyphicon glyphicon-envelope" arial-hiden="true"></span>
+                    </p>
+                    <p>
+                        <span class="input-group">
+                            <span class="input-group-addon" id="basic-addon2">
+                                <span class="glyphicon glyphicon-envelope" arial-hiden="true"></span>
+                            </span>
+                            <input type="email" name="email_contato" id="nome_contato" placeholder="digite o seu e-mail" aria-describedby="basic-addon2" required class="form-control">
                         </span>
-                        <input type="email" name="email_contato" id="nome_contato" placeholder="digite o seu e-mail" aria-describedby="basic-addon2" required class="form-control">
-                    </span>
-                </p>
-                <p>
-                    <span class="input-group">
-                        <span class="input-group-addon" id="basic-addon2">
-                            <span class="glyphicon glyphicon-pencil" arial-hiden="true"></span>
-                        </span>
-                        <textarea name="comentarios_contato" id="nome_contato" placeholder="digite o seus comentarios" aria-describedby="basic-addon3" required class="form-control"
-                        cols="30" rows="5">
+                    </p>
+                    <p>
+                        <span class="input-group">
+                            <span class="input-group-addon" id="basic-addon2">
+                                <span class="glyphicon glyphicon-pencil" arial-hiden="true"></span>
+                            </span>
+                            <textarea name="comentarios_contato" id="nome_contato" placeholder="digite o seus comentarios" aria-describedby="basic-addon3" required class="form-control" cols="30" rows="5">
                         </textarea>
-                    </span>
-                </p>
-                <p>
-                    <button class="btn btn-danger btn-block" aria-label="Enviar" role="Enviar">Enviar
-                        <span class="glyphicon glyphicon-send"></span>
-                    </button>
-                </p>
-            </form>
-        </div>
+                        </span>
+                    </p>
+                    <p>
+                        <button class="btn btn-danger btn-block" aria-label="Enviar" role="Enviar">Enviar
+                            <span class="glyphicon glyphicon-send"></span>
+                        </button>
+                    </p>
+                </form>
+            </div>
         </div> <!-- Abre área de contato -->
         <!-- Abre área de contato -->
-        <div class="panel-footer" >
-            <h6 class="text-danger">
-                Desenvolvido por Marconys Pinheiro; 2022.
-                <br>
-                <a href="https://github.com/marconys">
-                    marconys.com.br
-                </a>
-            </h6>
+        <div class="col-sm-12">
+            <div class="panel-footer">
+                <h6 class="text-danger text-center">
+                    Desenvolvido por Marconys Pinheiro; 2022.
+                    <br>
+                    <a href="https://github.com/marconys">
+                        marconys.com.br
+                    </a>
+                </h6>
+            </div>
         </div>
-
 
     </div>
 
