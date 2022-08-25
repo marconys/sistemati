5f497ea5c78d9e0d19e0464d4a26997cb7613044
Marconys <marconysbatera@gmail.com>
2022-08-25 13:28:34 -0300

    cadastro-se

--- client/cadastre-se.php
+++ client/cadastre-se.php
@@ -1,77 +1,60 @@
 <?php
-//Incluindo variáveis de ambiente e banco
+//Variaveis de ambiente
 include('../config.php');
+//Conexão com banco
 include('../conexoes/conexao.php');
 
 
-
-
-    if (isset($_POST['enviar'])) {
-
-    $nome_cliente = $_POST['nome_cliente'];        
-    $cpf_cliente = $_POST['cpf_cliente'];        
-    $email_cliente = $_POST['email_cliente'];        
-    $senha_cliente = $_POST['senha_cliente'];
-
-    $campos_insert = "nome_cliente,cpf_cliente,email_cliente,senha_cliente";
-    $values = "'$nome_cliente','$cpf_cliente','$email_cliente','$senha_cliente'";
+if($_POST){
     
-    $query = "insert into tbcliente ($campos_insert) values ($values);";
-    $resultado = $conexao->query($query);
+        if (isset($_POST['enviar'])) {
+            $nome_img = $_FILES['foto_cliente']['name'];
+            $tmp_img = $_FILES['imagem_produto']['tmp_name'];
+            $pasta_img = "../images/" . $nome_img;
+            move_uploaded_file($tmp_img, $pasta_img);
+        }
+    
 
- // var_dump($$query);
+        $nome_cliente = $_POST['nome_cliente'];
+        $cpf_cliente = $_POST['cpf_cliente'];        
+        $email_cliente = $_POST['email_cliente'];        
+        $senha_cliente = $_POST['senha_cliente'];       
+        $foto_cliente = $_FILES['foto_cliente']['name'];
 
-//Após o insert direciona a pagina
-    if(mysqli_insert_id($conexao)){
-        header("location: ../admin/index.php");
+        $campos_insert = "nome_cliente,cpf_cliente,email_cliente,senha_cliente,foto_cliente";
+        $values = "$nome_cliente,'$cpf_cliente','$email_cliente','$senha_cliente','$foto_cliente'";
+        
+        $query = "insert into tbcliente ($campos_insert) values ($values);";
+        $resultado = $conexao->query($query);
+
+     // var_dump($$query);
+
+    //Após o insert direciona a pagina
+   if(mysqli_insert_id($conexao)){
+        header("location: ../admin/login.php");
     }else{
-        header("location: ../index.php");
-    }
-}    
+        header("location: ../admin/login.php");
+    } 
+}
 ?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> <?php echo SYS_NAME; ?> - Cadastro</title>
-    <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css">
-    <link href="../css/meu_estilo.css" rel="stylesheet" type="text/css">
+    <title>
+        <<?php echo SYS_NAME; ?> - Cadastre-se </title>
+            <link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css">
+            <link rel="stylesheet" href="../css/meu_estilo.css" type="text/css">
 </head>
 
-<body>
-    <nav class="nav navbar-inverse">
-        <div class="container-fluid">
-            <!-- Agrupamento para exibição em mobile-->
-            <div class="navbar-header">
-                <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#defaultNavbar" aria-expanded="false">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a href="index.php" class="navbar-brand">
-                    <img src="../images/logochurrascopequeno.png" alt="">
-                </a>
-            </div> <!-- Fecha o grupamento para exibição em mobile-->
-            <div class="collapse navbar-collapse" id="#defaultNavbar">
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="active"><a href="../index.php"><span class="glyphicon glyphicon-home"></span></a></li>
-                    <li><a href="../index.php#produtos">Produtos</a></li>
-                    <li><a href="../index.php#destaques">Destaques</a></li>
-                    <li><a href="../index.php#reservas">Reserva</a></li>
-                    <li><a href="../index.php#contato">Contato</a></li>
-                </ul>
-            </div> <!-- Fecha nav a direita-->
-        </div>
-    </nav> <!-- Fecha barra de navegação-->
+<body class="fundofixo">
+    <?php include('../menu_publico.php'); ?>
     <main class="container">
         <div class="row">
-            <!-- Abre dimensionamento -->
-            <div class="col-xs-12 col-sm-offset-3 col-sm-3 col-md-offset- col-md-6">
-                <h2 class="breadcrumb tex-danger">
+            <div class="col-xs-12 col-sm-offset-3 col-sm-6 col-md-offset-2 col-md-8">
+                <h2 class="breadcrumb tex-primary">
                     <a href="../index.php">
                         <button class="btn btn-danger">
                             <span class="glyphicon glyphicon-chevron-left"></span>
@@ -79,65 +62,102 @@ include('../conexoes/conexao.php');
                     </a>
                     Cadastre-se
                 </h2>
-
-
-                <!-- Abre thumbnail -->
                 <div class="thumbnail">
-                    <div class="alert alert-success" role="alert">
-                        <!-- Abre formulário -->
-                        <form action="cadastre-se.php" method="post" id="form_cadastre-se" name="form_cadastre-se" enctype="text/plain">
-                            <label for="nome_cliente">NOME COMPLETO:</label>
+                    <!-- Abre thumbnail -->
+                    <div class="alert alert-info" role="alert">
+                        <form action="cadastre-se.php" method="post" id="form_cadastre-se" name="form_cadastre-se" enctype="multipart/form-data">
+                           
+                            <!-- Text nome_cliente -->
+                            <label for="nome_cliente">Nome Completo:</label>
                             <div class="input-group">
                                 <span class="input-group-addon">
                                     <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
                                 </span>
-                                <input type="text" class="form-control" id="nome_cliente" name="nome_cliente" maxlength="100" required placeholder="Digite o seu nome Completo...">
+                                <input type="text" class="form-control" id="nome_cliente" name="nome_cliente" maxlength="100" required  placeholder="Digite o seu nome e sobrenome...">
                             </div>
                             <br>
+                            <!-- Text cpf_cliente -->
                             <label for="cpf_cliente">CPF:</label>
                             <div class="input-group">
                                 <span class="input-group-addon">
-                                    <span class="glyphicon glyphicon-credit-card" aria-hidden="true"></span>
+                                    <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
                                 </span>
-                                <input type="text" class="form-control" id="cpf_cliente" name="cpf_cliente" maxlength="11" required placeholder="Digite o seu CPF sem pontos e traços...">
+                                <input type="text" class="form-control" id="cpf_cliente" name="cpf_cliente" maxlength="12" required  placeholder="Digite o seu CPF sem traçoes e prontos...">
                             </div>
                             <br>
-                            <label for="email_cliente">EMAIL</label>
+                            <!-- number email_cliente -->
+                            <label for="email_cliente">E-mail:</label>
                             <div class="input-group">
                                 <span class="input-group-addon">
                                     <span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>
                                 </span>
-                                <input type="text" class="form-control" id="email_cliente" name="email_cliente" maxlength="100" required placeholder="email@email.com.br">
+                                <input type="text" name="email_cliente" id="email_cliente" required placeholder="Ex: email@email.com..." class="form-control">
                             </div>
                             <br>
-                            <label for="">SENHA</label>
+                            <!-- number senha_cliente -->
+                            <label for="senha_cliente">Senha:</label>
                             <div class="input-group">
                                 <span class="input-group-addon">
                                     <span class="glyphicon glyphicon-lock" aria-hidden="true"></span>
                                 </span>
-                                <input type="password" class="form-control" id="email_cliente" name="email_cliente" maxlength="12" required placeholder="Digite a sua senha">
+                                <input type="password" name="senha_cliente" id="senha_cliente" required placeholder="Digite a sua senha..." class="form-control">
                             </div>
                             <br>
-                            <input type="submit" value="Cadastrar" name="enviar" id="enviar" class="btn btn-danger btn-block">
+                            <!-- file imagem-->
+                            <label for="foto_cliente">Foto:</label>
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <span class="glyphicon glyphicon-picture" aria-hidden="true"></span>
+                                </span>
+                                <img src="" alt="" name="foto" id="foto" class="img-responsive">
+                                <input type="file" name="foto_cliente" id="foto_cliente" class="form-control" accept="image/*">
+                            </div>
+                            <br>
+                            <!-- Botão Enviar -->
+                            <input type="submit" value="Cadastrar" name="enviar" id="enviar" class="btn btn-success btn-block">
                         </form>
-                        <!-- fecha formulário -->
-                        <!-- Fecha alert-sucess -->
                     </div>
-                    <!-- Fecha thumbnail -->
                 </div>
-                <!-- Fecha dimensionamento -->
             </div>
-
         </div>
 
-            <footer>
-                <?php include('../rodape.php'); ?>
-            </footer>
+        <footer>
+            <?php include('../rodape.php'); ?>
+        </footer>
+
     </main>
-    <!-- Link arquivos Bootstrap js -->
+    <!-- Script para a imagem -->
+    <script>
+        document.getElementById("foto_cliente").onchange = function() {
+            var reader = new FileReader();
+            if (this.files[0].size > 528385) {
+                alert("A imagem deve ter no máximo 500KB");
+                $("#foto").attr("src", "blank");
+                $("#foto").hide();
+                $("#foto_cliente").wrap('<form>').closest('form').get(0).reset();
+                $("#foto_cliente").unwrap();
+                return false;
+
+            }
+            // Verifica se o input do titpo file possui dado
+            if (this.files[0].type.indexOf("image") == -1) {
+                alert("Formato inválido, escolha uma imagem!");
+                $("#foto").attr("src", "blank");
+                $("#foto").hide();
+                $("#foto_cliente").wrap('<form>').closest('form').get(0).reset();
+                $("#foto_cliente").unwrap();
+                return false;
+            };
+            reader.onload = function(e) {
+                //Obter dados  carregados e renderizar a miniatura
+                document.getElementById("foto").src = e.target.result;
+                $("#foto").show();
+            };
+            reader.readAsDataURL(this.files[0]);
+        };
+    </script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="../js/bootstrap.min.js"></script>
-
 </body>
 
 </html>
