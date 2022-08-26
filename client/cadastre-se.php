db13017f786a848f275595c36cadbde337023c83
Marconys <marconysbatera@gmail.com>
2022-08-25 23:03:46 -0300

    inserido o arquivo cliente_insere_dados.php

--- client/cadastre-se.php
+++ client/cadastre-se.php
@@ -1,42 +1,4 @@
-<?php
-//Variaveis de ambiente
-include('../config.php');
-//Conexão com banco
-include('../conexoes/conexao.php');
 
-
-if($_POST){
-    
-        if (isset($_POST['enviar'])) {
-            $nome_img = $_FILES['foto_cliente']['name'];
-            $tmp_img = $_FILES['imagem_produto']['tmp_name'];
-            $pasta_img = "../images/" . $nome_img;
-            move_uploaded_file($tmp_img, $pasta_img);
-        }
-    
-
-        $nome_cliente = $_POST['nome_cliente'];
-        $cpf_cliente = $_POST['cpf_cliente'];        
-        $email_cliente = $_POST['email_cliente'];        
-        $senha_cliente = $_POST['senha_cliente'];       
-        $foto_cliente = $_FILES['foto_cliente']['name'];
-
-        $campos_insert = "nome_cliente,cpf_cliente,email_cliente,senha_cliente,foto_cliente";
-        $values = "$nome_cliente,'$cpf_cliente','$email_cliente','$senha_cliente','$foto_cliente'";
-        
-        $query = "insert into tbcliente ($campos_insert) values ($values);";
-        $resultado = $conexao->query($query);
-
-     // var_dump($$query);
-
-    //Após o insert direciona a pagina
-   if(mysqli_insert_id($conexao)){
-        header("location: ../admin/login.php");
-    }else{
-        header("location: ../admin/login.php");
-    } 
-}
-?>
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -65,7 +27,7 @@ if($_POST){
                 <div class="thumbnail">
                     <!-- Abre thumbnail -->
                     <div class="alert alert-info" role="alert">
-                        <form action="cadastre-se.php" method="post" id="form_cadastre-se" name="form_cadastre-se" enctype="multipart/form-data">
+                        <form action="cliente_insere_dados.php" method="post" id="form_cliente_insere_dados" name="form_cliente_insere_dados" enctype="multipart/form-data">
                            
                             <!-- Text nome_cliente -->
                             <label for="nome_cliente">Nome Completo:</label>
