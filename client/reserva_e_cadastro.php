882f6d28c0dc0ce6327f7ed647e39fb39eecdb18
Marconys <marconysbatera@gmail.com>
2022-08-30 15:32:43 -0300

    Inserido regras de negocio/prazo mínimo e máximo de antecedência

--- client/reserva_e_cadastro.php
+++ client/reserva_e_cadastro.php
@@ -11,66 +11,82 @@ include('../conexoes/conexao2.php');
 //atribui os campos recebidos a uma váriavél
 $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 
-
-//confere se realmente esta recebendo os dados
-//var_dump($dados);
-try{
-
-
-//Verifica se o usuário clicou no botão
-if(!empty($dados['enviar'])){
-    //Verifica a quantidade de pessoas para aplicar o valor promocional
-    if($dados['numero_pessoas_reserva'] >= 5){
-        $valor_reserva = 29.90;
-    }else{
-        $valor_reserva = 99.90;
+//Verifica a data escolhida pelo cliente
+if (isset($dados['data_reserva'])) {
+    //Calcula a diferença de dias entre a data atual e a data da reserva
+    $hoje = new DateTime('now');
+    $data_reserva = new DateTime($dados['data_reserva']);
+    $interval = $data_reserva->diff($hoje);
+    $dias = $interval->days;
     }
-
-    //inseridodados na tabela tbcliente
-    $query_cliente =  "INSERT INTO tbcliente (nome_cliente, cpf_cliente, email_cliente, senha_cliente) 
-    VALUES (:nome_cliente, :cpf_cliente, :email_cliente, :senha_cliente )";
-    //Prepara a query e atribui a variavel $cad_cliente
-    $cad_cliente = $conexao2->prepare($query_cliente);
-    //Substitui o link pelos valores armazenados na variavel $dados
-    $cad_cliente->bindParam(':nome_cliente',$dados['nome_cliente'], PDO::PARAM_STR);
-    $cad_cliente->bindParam(':cpf_cliente',$dados['cpf_cliente'], PDO::PARAM_STR);
-    $cad_cliente->bindParam(':email_cliente',$dados['email_cliente'], PDO::PARAM_STR);
-    $cad_cliente->bindParam(':senha_cliente',$dados['senha_cliente'], PDO::PARAM_STR);
-    //executa a query
-    $cad_cliente->execute();
-    //Recupera o último ID inserido
-    //var_dump($conexao2->lastInsertId());
-
-    //Atribui o ID capturado a variavel $id_cliente para ser usado na reserva
-    $id_cliente = $conexao2->lastInsertId();
-
-    //inseridodados na tabela tbcliente
-    $query_reserva =  "INSERT INTO tbreserva (data_reserva, hora_reserva, numero_pessoas_reserva, motivo_reserva, id_cliente_reserva, valor_reserva) 
-    VALUES (:data_reserva, :hora_reserva, :numero_pessoas_reserva, :motivo_reserva, :id_cliente_reserva, $valor_reserva)";
-    //Prepara a query e atribui a variavel $cad_reserva
-    $cad_reserva = $conexao2->prepare($query_reserva);
-    //Substitui o link pelos valores armazenados na variavel $dados
-    $cad_reserva->bindParam(':data_reserva',$dados['data_reserva'], PDO::PARAM_STR);
-    $cad_reserva->bindParam(':hora_reserva',$dados['hora_reserva'], PDO::PARAM_STR);
-    $cad_reserva->bindParam(':numero_pessoas_reserva',$dados['numero_pessoas_reserva'], PDO::PARAM_STR);
-    $cad_reserva->bindParam(':motivo_reserva',$dados['motivo_reserva'], PDO::PARAM_STR);
-    $cad_reserva->bindParam(':id_cliente_reserva',$id_cliente, PDO::PARAM_INT);
-    //executa a query
-    $cad_reserva->execute();     
-    
-
+//Verifica se o prazo da reserva atende o mínimo de 12 e máximo de  60 dias
+if ($dias < 12 || $dias > 60) {
     //Criar variavel global para  salvar  a mensagem de sucesso
-    $_SESSION['msg'] = "<p style='color:green;'> Usuário cadastrado com sucesso!<br>Use o seu e-mail, CPF e senha para fazer login!</p>";
-
-    //Redireciona a págia
-
-    header("location: ../cadastro_e_reserva.php");    
-}
-
-} catch(Exception){
-    //Redireciona a págia
+    $_SESSION['msg'] = "<p style='color:#f00;'> O prazo mínimo de antecedência é de 12 dias e o máxio 60 dias!</p>";
     header('location: ../cadastro_e_reserva.php');
-    //Criar variavel global para  salvar  a mensagem de erro
-    $_SESSION['msg'] = "<p style='color:#f00;'> Erro: Ops!! Parece que o e-mail ou CPF já estão cadastrados!</p>";
-}
+
+    }else{
+        //confere se realmente esta recebendo os dados
+        //var_dump($dados);
+        try{
+
+
+        //Verifica se o usuário clicou no botão
+        if(!empty($dados['enviar'])){
+            //Verifica a quantidade de pessoas para aplicar o valor promocional
+            if($dados['numero_pessoas_reserva'] >= 5){
+                $valor_reserva = 29.90;
+            }else{
+                $valor_reserva = 99.90;
+            }
+
+            //inseridodados na tabela tbcliente
+            $query_cliente =  "INSERT INTO tbcliente (nome_cliente, cpf_cliente, email_cliente, senha_cliente) 
+            VALUES (:nome_cliente, :cpf_cliente, :email_cliente, :senha_cliente )";
+            //Prepara a query e atribui a variavel $cad_cliente
+            $cad_cliente = $conexao2->prepare($query_cliente);
+            //Substitui o link pelos valores armazenados na variavel $dados
+            $cad_cliente->bindParam(':nome_cliente',$dados['nome_cliente'], PDO::PARAM_STR);
+            $cad_cliente->bindParam(':cpf_cliente',$dados['cpf_cliente'], PDO::PARAM_STR);
+            $cad_cliente->bindParam(':email_cliente',$dados['email_cliente'], PDO::PARAM_STR);
+            $cad_cliente->bindParam(':senha_cliente',$dados['senha_cliente'], PDO::PARAM_STR);
+            //executa a query
+            $cad_cliente->execute();
+            //Recupera o último ID inserido
+            //var_dump($conexao2->lastInsertId());
+
+            //Atribui o ID capturado a variavel $id_cliente para ser usado na reserva
+            $id_cliente = $conexao2->lastInsertId();
+
+            //inseridodados na tabela tbcliente
+            $query_reserva =  "INSERT INTO tbreserva (data_reserva, hora_reserva, numero_pessoas_reserva, motivo_reserva, id_cliente_reserva, valor_reserva) 
+            VALUES (:data_reserva, :hora_reserva, :numero_pessoas_reserva, :motivo_reserva, :id_cliente_reserva, $valor_reserva)";
+            //Prepara a query e atribui a variavel $cad_reserva
+            $cad_reserva = $conexao2->prepare($query_reserva);
+            //Substitui o link pelos valores armazenados na variavel $dados
+            $cad_reserva->bindParam(':data_reserva',$dados['data_reserva'], PDO::PARAM_STR);
+            $cad_reserva->bindParam(':hora_reserva',$dados['hora_reserva'], PDO::PARAM_STR);
+            $cad_reserva->bindParam(':numero_pessoas_reserva',$dados['numero_pessoas_reserva'], PDO::PARAM_STR);
+            $cad_reserva->bindParam(':motivo_reserva',$dados['motivo_reserva'], PDO::PARAM_STR);
+            $cad_reserva->bindParam(':id_cliente_reserva',$id_cliente, PDO::PARAM_INT);
+            //executa a query
+            $cad_reserva->execute();     
+            
+
+            //Criar variavel global para  salvar  a mensagem de sucesso
+            $_SESSION['msg'] = "<p style='color:green;'> Usuário cadastrado com sucesso!<br>Use o seu e-mail, CPF e senha para fazer login!</p>";
+
+            //Redireciona a págia
+
+            header("location: ../cadastro_e_reserva.php");    
+        }
+
+        } catch(Exception){
+            //Redireciona a págia
+            header('location: ../cadastro_e_reserva.php');
+            //Criar variavel global para  salvar  a mensagem de erro
+            $_SESSION['msg'] = "<p style='color:#f00;'> Erro: Ops!! Parece que o e-mail ou CPF já estão cadastrados!</p>";
+        }
+
+    }
 ?>
